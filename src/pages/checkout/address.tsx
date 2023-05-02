e1441870a6ededef24c836b51ba9dfe732dbfdca
Williams Mata <williams.rm99@gmail.com>
2023-05-02 15:43:51 -0400

    style beta done

---
+++ src/pages/checkout/address.tsx
@@ -0,0 +1,69 @@
+import { NextPage } from "next";
+import {
+  Box,
+  Button,
+  FormControl,
+  Grid,
+  MenuItem,
+  Select,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { ShopLayout } from "@/components/layouts";
+
+const AddressPage: NextPage = () => {
+  return (
+    <ShopLayout
+      title="Checkout"
+      pageDescription="Confirm destination address"
+      maxWidth="lg"
+    >
+      <Typography variant="h1">Address</Typography>
+
+      <Grid container spacing={2} sx={{ mt: 2 }}>
+        <Grid item xs={12} sm={6}>
+          <TextField label="Name" variant="filled" fullWidth />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField label="Last Name" variant="filled" fullWidth />
+        </Grid>
+
+        <Grid item xs={12} sm={6}>
+          <TextField label="Street address" variant="filled" fullWidth />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField label="Street address 2" variant="filled" fullWidth />
+        </Grid>
+
+        <Grid item xs={12} sm={6}>
+          <TextField label="Postal code" variant="filled" fullWidth />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField label="City" variant="filled" fullWidth />
+        </Grid>
+
+        <Grid item xs={12} sm={6}>
+          <FormControl fullWidth>
+            <Select variant="filled" label="Country" value={1}>
+              <MenuItem value={1}>Costa Rica</MenuItem>
+              <MenuItem value={2}>Honduras</MenuItem>
+              <MenuItem value={3}>Mexico</MenuItem>
+              <MenuItem value={4}>Venezuela</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField label="Phone number" variant="filled" fullWidth />
+        </Grid>
+      </Grid>
+
+      <Box sx={{ mt: 4 }} display="flex" justifyContent="center">
+        <Button color="secondary" className="circular-btn" size="large">
+          Checkout
+        </Button>
+      </Box>
+    </ShopLayout>
+  );
+};
+
+export default AddressPage;
