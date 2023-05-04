4588fd2ea9f64af247be2d7e85d1d8581785d16a
Williams Mata <williams.rm99@gmail.com>
2023-05-03 21:45:40 -0400

    search by title or tags done

---
+++ src/pages/api/search/[q].ts
@@ -0,0 +1,68 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "@/server";
+
+type Data =
+  | {
+      message: string;
+    }
+  | GetProductSearch[];
+
+interface GetProductSearch {
+  images: { url: string }[];
+  title: string;
+  inStock: number;
+  price: number;
+  slug: string;
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  switch (req.method) {
+    case "GET":
+      return searchProducts(req, res);
+
+    default:
+      return res.status(400).json({ message: "Bad request" });
+  }
+}
+
+async function searchProducts(req: NextApiRequest, res: NextApiResponse<Data>) {
+  let { q = "" } = req.query;
+
+  if (q.length === 0) {
+    return res.status(400).json({
+      message: "You must specify the search query",
+    });
+  }
+
+  q = q.toString().toLowerCase();
+
+  try {
+    const products = await prisma.product.findMany({
+      where: {
+        OR: [
+          { title: { contains: q } },
+          { tags: { some: { name: { contains: q } } } },
+        ],
+      },
+
+      select: {
+        title: true,
+        price: true,
+        inStock: true,
+        slug: true,
+        images: { select: { url: true } },
+      },
+    });
+
+    return res.status(200).json(products);
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).json({
+      message: "Please talk to the admin",
+    });
+  }
+}
